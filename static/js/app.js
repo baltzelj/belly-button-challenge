24b94d7449f56e1b68f8727055da34dacec56e8a
baltzelj <baltzelj@gmail.com>
2024-08-13 11:00:41 -0700

    minor changes and updates to get started.

--- static/js/app.js
+++ static/js/app.js
@@ -3,10 +3,10 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    metadata = data['metadata']
 
     // Filter the metadata for the object with the desired sample number
-
+    
 
     // Use d3 to select the panel with id of `#sample-metadata`
 
